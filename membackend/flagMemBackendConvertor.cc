f3c4409a4cd238faee764ce180acb8872ff7bda8
sabbirahm3d <sabbir0ahmed0@gmail.com>
2019-10-18 13:53:52 -0400

    copy files

---
+++ membackend/flagMemBackendConvertor.cc
@@ -0,0 +1,61 @@
+// Copyright 2009-2019 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2019, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+
+#include <sst/core/sst_config.h>
+#include "util.h"
+#include "memoryController.h"
+#include "membackend/flagMemBackendConvertor.h"
+#include "membackend/memBackend.h"
+
+using namespace SST;
+using namespace SST::MemHierarchy;
+
+#ifdef __SST_DEBUG_OUTPUT__
+#define Debug(level, fmt, ... ) m_dbg.debug( level, fmt, ##__VA_ARGS__  )
+#else
+#define Debug(level, fmt, ...)
+#endif
+
+FlagMemBackendConvertor::FlagMemBackendConvertor(Component *comp, Params &params) :
+    MemBackendConvertor(comp, params) {
+    using std::placeholders::_1;
+    using std::placeholders::_2;
+    static_cast<FlagMemBackend *>(m_backend)->setResponseHandler(
+        std::bind(&FlagMemBackendConvertor::handleMemResponse, this, _1, _2));
+}
+
+FlagMemBackendConvertor::FlagMemBackendConvertor(ComponentId_t id, Params &params) :
+    MemBackendConvertor(id, params) {
+    using std::placeholders::_1;
+    using std::placeholders::_2;
+    static_cast<FlagMemBackend *>(m_backend)->setResponseHandler(
+        std::bind(&FlagMemBackendConvertor::handleMemResponse, this, _1, _2));
+}
+
+bool FlagMemBackendConvertor::issue(BaseReq *breq) {
+    if (breq->isMemEv()) {
+        MemReq *req = static_cast<MemReq *>(breq);
+        MemEvent *event = req->getMemEvent();
+        return static_cast<FlagMemBackend *>(m_backend)->issueRequest(req->id(), req->addr(),
+                                                                      req->isWrite(),
+                                                                      event->getFlags(),
+                                                                      m_backendRequestWidth);
+    } else {
+        CustomReq *req = static_cast<CustomReq *>(breq);
+        return static_cast<FlagMemBackend *>(m_backend)->issueCustomRequest(req->id(),
+                                                                            req->getInfo());
+    }
+}
---
+++ membackend/flagMemBackendConvertor.h
@@ -0,0 +1,55 @@
+// Copyright 2009-2019 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2019, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+
+#ifndef __SST_MEMH_FLAGMEMBACKENDCONVERTOR__
+#define __SST_MEMH_FLAGMEMBACKENDCONVERTOR__
+
+#include "memBackendConvertor.h"
+
+namespace SST {
+    namespace MemHierarchy {
+
+        class FlagMemBackendConvertor : public MemBackendConvertor {
+        public:
+/* Element Library Info */
+            SST_ELI_REGISTER_SUBCOMPONENT_DERIVED_API(SST::MemHierarchy::FlagMemBackendConvertor, SST::MemHierarchy::MemBackendConvertor
+            )
+
+            SST_ELI_REGISTER_SUBCOMPONENT_DERIVED(FlagMemBackendConvertor,
+            "memHierarchy", "flagMemBackendConvertor", SST_ELI_ELEMENT_VERSION(1,0,0),
+            "Convert MemEventBase* for a FlagMemBackend - accepts and returns the 'flags' field", SST::MemHierarchy::MemBackendConvertor)
+
+            SST_ELI_DOCUMENT_PARAMS( MEMBACKENDCONVERTOR_ELI_PARAMS )
+
+            SST_ELI_DOCUMENT_STATISTICS( MEMBACKENDCONVERTOR_ELI_STATS )
+
+            SST_ELI_DOCUMENT_SUBCOMPONENT_SLOTS( MEMBACKENDCONVERTOR_ELI_SLOTS )
+
+/* Begin class definition */
+            FlagMemBackendConvertor(Component *comp, Params &params);
+
+            FlagMemBackendConvertor(ComponentId_t id, Params &params);
+
+            virtual bool issue(BaseReq *req);
+
+            virtual void handleMemResponse(ReqId reqId, uint32_t flags) {
+                doResponse(reqId, flags);
+            }
+        };
+
+    }
+}
+#endif
